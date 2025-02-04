54534c0dda01edbcd0dad0eb1874ad44a670b150
Aniketh M B <anikethgowda022@gmail.com>
2025-02-04 22:33:10 +0530

    Added DTOs

--- Controllers/RegionController.cs
+++ Controllers/RegionController.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using NzWalks.Data;
 using NzWalks.Models.Domain;
+using NzWalks.Models.DTOs;
 
 namespace NzWalks.Controllers
 {
@@ -22,9 +23,54 @@ namespace NzWalks.Controllers
         }
         [HttpGet]
         public IActionResult GetAll()
-        {
+        {   
+            //Get data from database
             var regions = _dbContext.RegionsSet.ToList();
-            return Ok(regions);
+
+            //Map models and DTOs
+            var regionDto = new List <RegionDto>();
+            foreach (var region in regions)
+            {
+                regionDto.Add(new RegionDto()
+                {   ID = region.ID,
+                    Code = region.Code,
+                    Name = region.Name,
+                    RegionImgUrl=region.RegionImgUrl,
+                });
+            }
+            
+
+
+            //Return DTOs
+            return Ok(regionDto);
+        }
+
+
+        //Function to get single regions
+        [HttpGet]
+        [Route("{code}")]
+        public IActionResult GetRegionById(string code)
+        {
+            // get domain model data  from database
+            var  region = _dbContext.RegionsSet.FirstOrDefault(x => x.Code == code);
+
+
+            if(region == null)
+            {
+                return NotFound();
+            }
+
+            //map domain model to dto
+
+            var regionDto = new RegionDto{
+                ID = region.ID,
+                Code = region.Code,
+                Name = region.Name,
+                RegionImgUrl= region.RegionImgUrl,
+            };
+
+
+            return Ok(regionDto);
         }
     }
 }
